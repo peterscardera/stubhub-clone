21333fa72edefc3658423de6e9b0b6ec9e0e4083
PeterScardera <peterscardera@gmail.com>
2020-06-21 16:05:58 -0400

    google cloud added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # stubhub-clone
+
+'kubectl get all' to see all applications and services on tthe cluster
+
+ingress-ngnix used so anything heading to /api/users/ flows to the auth-srv
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import bodyParser from "body-parser";
 const app = express();
 app.use(bodyParser.json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("hi the");
+});
+
 app.listen(3000, () => {
   console.log("listening on posrt 3000!!!");
 });
